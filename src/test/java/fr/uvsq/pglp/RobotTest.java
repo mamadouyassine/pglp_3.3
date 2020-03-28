00e0e85fd53d72aec3206166de5637c4b5cdef72
uvsq21920982 <yassinemamadou97@gmail.com>
2020-03-28 14:01:45 +0100

    MAJ robostatique

--- src/test/java/fr/uvsq/pglp/RobotTest.java
+++ src/test/java/fr/uvsq/pglp/RobotTest.java
@@ -4,7 +4,7 @@ public class RobotTest {
     public void test() {
         Robot r1 = new Robot(10);
         RobotMobile rm1 = new RobotMobile(20);
-        RobotStatique rs=new RobotStatique(0);
+        RobotStatique rs1 =new RobotStatique(0);
 
         //assertTrue(rm1.avanceRTous());
     }
